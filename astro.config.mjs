b24562f15da3b9ccb8620a785897ce7f88b6ecd1
Samgu Lee <cable8mm@gmail.com>
2026-05-29 11:39:22 +0900

    chore: set dev environment (#7)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,19 @@ import tailwind from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://labs.etern.co.kr',
+  
+  // Astro의 기본 개발 서버 환경 설정 (Nginx/Herd가 바라볼 수 있도록 문을 열어줍니다)
+  server: {
+    host: true,
+  },
+
   vite: {
     plugins: [tailwind()],
+    
+    // Nginx(Herd) 호스트 허용 설정 (Vite 6 내부 보안 정책 우회)
+    server: {
+      strictPort: true,
+      allowedHosts: true, // labs.etern.co.kr.test 접속 허용
+    },
   },
 });
