96702286cc39b14b246d49cde9290165006c07a6
Samgu Lee <cable8mm@gmail.com>
2026-05-30 12:12:14 +0900

    feat: install `playwright` and `vitest` for testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,7 @@ const baseURL = `http://localhost:${PORT}`;
 // See https://playwright.dev/docs/test-configuration.
 export default defineConfig({
   testDir: "./tests",
+  testMatch: "/*.e2e.ts",
   // Fail the build on CI if you accidentally left test.only in the source code.
   forbidOnly: !!process.env.CI,
   // Retry on CI only.
@@ -20,6 +21,6 @@ export default defineConfig({
   },
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
-    baseURL,
+    baseURL: "http://localhost:4321/labs/",
   },
 });
